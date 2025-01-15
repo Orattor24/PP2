b1afb3e8fa64a2cc681ccc298d227d706ec86436
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2025-01-15 12:02:12 +0500

    What I did?

---
+++ git/booleans.py
@@ -0,0 +1,35 @@
+print(10 > 9)
+print(10 ==9)
+print(10 < 9)
+
+#Second example
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+#Bool
+print(bool("Hello"))
+print(bool(15))
+#Bool exp. 2
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#Most Values are True
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+#Some Values are False
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
