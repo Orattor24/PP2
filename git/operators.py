f250ab8d4e096e0010f2a882e09f7c8187358004
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2025-01-15 12:13:52 +0500

    Operators . navernoe

---
+++ git/operators.py
@@ -0,0 +1,10 @@
+print(10 + 5)
+'''Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators'''
+print((6 + 3) - (6 + 3))
+print(100 + 5 * 3)
